21e938cc3b0a6ee6d90051a9a6c8e6bbee87b9c2
Andrii-Rodchenko <rod4enkoandriy@gmail.com>
2022-05-30 23:02:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,41 +5,43 @@ const formEl = document.querySelector(".feedback-form")
 const inputEl = document.querySelector(".feedback-form input")
 const textareaEl = document.querySelector(".feedback-form textarea")
 
-
 formEl.addEventListener("input", throttle(handleFormInput, 500))
-document.addEventListener("DOMContentLoaded", handleLoadingPage);
+
 formEl.addEventListener("submit", handleFormSubmission)
-const STORAGEKEY = "feedback-form-state";
-const formData = {
-    email: "",
-    message: ""};
+
+const STORAGE_KEY = "feedback-form-state";
+const localStorageData = localStorage.getItem(STORAGE_KEY);
+const objectWithLocalStorage = JSON.parse(localStorageData) || "";
+
+const formData = { };
+
+
+function handleFormInput({ target }) { 
+    if (target.name !== "email")
+    {
+        formData.email = inputEl.value;
+    }
+    else if (target.name !== "message") {
+         formData.message = textareaEl.value;
+}
+    formData[target.name] = target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+handleLoadingPage()
 
 function handleLoadingPage() {
-    const localStorageData = localStorage.getItem(STORAGEKEY )
-    if (localStorageData !== "") {
-        try { const result = JSON.parse(localStorage.getItem(STORAGEKEY ))
-            inputEl.value = result.email;
-            textareaEl.value = result.message;}
-        catch { return null };
+        try { inputEl.value = objectWithLocalStorage.email  ;
+            textareaEl.value = objectWithLocalStorage.message ;
+    } 
+        catch { return console.log(null)  };
         }
-    else {
-        inputEl.value = "";
-        textareaEl.value = ""; 
-    }
-}
-
-function handleFormInput({target}) { 
-    formData[target.name] = target.value;
-    localStorage.setItem(STORAGEKEY, JSON.stringify(formData));
-
-}
 
 function handleFormSubmission(event) {
     event.preventDefault() 
     console.log(formData)
     localStorage.clear();
-    inputEl.value = "";
-    textareaEl.value = "";
+    formEl.reset()
 }
 
 
